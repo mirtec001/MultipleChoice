1a16e5005d1f9d4de4c3fa1612ba2a29ab4f4fa2
Pete Misik <mirtec001@hotmail.com>
2019-11-14 14:25:26 -0500

    Uploaded initial commit of application

---
+++ Model/DataModel.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MultipleChoiceMVVM.Model
+{
+
+    public class DataModel
+    {
+        
+        public List<List<string>> ConnectToDataBase(string datasource)
+        {
+            List<List<string>> data = new List<List<string>>();
+            using (SQLiteConnection conn = new SQLiteConnection(@"DataSource=" + datasource))
+            {
+                conn.Open();
+                SQLiteCommand command = new SQLiteCommand("Select * from Questions", conn);
+
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    var ndxPrimaryKey = reader.GetOrdinal("Index");
+                    var ndxQuestion = reader.GetOrdinal("Question");
+                    var ndxAnswer1 = reader.GetOrdinal("Answer1");
+                    var ndxAnswer2 = reader.GetOrdinal("Answer2");
+                    var ndxAnswer3 = reader.GetOrdinal("Answer3");
+                    var ndxAnswer4 = reader.GetOrdinal("Answer4");
+
+                    while (reader.Read())
+                    {
+                        List<string> record = new List<string>();
+                        record.Add(reader.GetValue(ndxPrimaryKey) as string);
+                        record.Add(reader.GetValue(ndxQuestion) as string);
+                        record.Add(reader.GetValue(ndxAnswer1) as string);
+                        record.Add(reader.GetValue(ndxAnswer2) as string);
+                        record.Add(reader.GetValue(ndxAnswer3) as string);
+                        record.Add(reader.GetValue(ndxAnswer4) as string);
+
+                        data.Add(record);
+                    }
+
+                }
+
+                conn.Close();
+
+                // string message = data[0][1] + "\n" + data[0][2] + "\n" + data[0][3] + "\n" + data[0][4] + "\n" + data[0][5];
+                // MessageBox.Show(message, "Question 0", MessageBoxButton.OK);
+                return data;
+            }
+        }
+
+        public void SaveQuestionsToDatabase(List<string> stream, string databasename)
+        {
+            
+
+            for (int i = 0; i < stream.Count / 6; i += 6)
+            {
+                List<string> tempArray = new List<string>();
+                // Array is laid out as 
+                // [0] Question
+                // [1] AnswerA
+                // [2] AnswerB
+                // [3] AnswerC
+                // [4] AnswerD
+                // [5] CorrectAnswer
+
+                /*
+                 * The table is laid out as 
+                 * [0] primary key
+                 * [1] Question
+                 * [2] AnswerA (Correct Answer)
+                 * [3] AnswerB
+                 * [4] AnswerC
+                 * [5] AnswerD
+                 * 
+                 * To get the two to mesh, we need to figure out the correct answer and put it in to slot 1 of our temp array
+                 */
+
+                tempArray.Add(stream[i + 0]);
+
+                switch (stream[i + 5].ToLower())
+                {
+                    case "a":
+                        tempArray.Add(stream[i + 1]);
+                        tempArray.Add(stream[i + 2]);
+                        tempArray.Add(stream[i + 3]);
+                        tempArray.Add(stream[i + 4]);
+                        break;
+                    case "b":
+                        tempArray.Add(stream[i + 2]);
+                        tempArray.Add(stream[i + 3]);
+                        tempArray.Add(stream[i + 4]);
+                        tempArray.Add(stream[i + 1]);
+                        break;
+                    case "c":
+                        tempArray.Add(stream[i + 3]);
+                        tempArray.Add(stream[i + 4]);
+                        tempArray.Add(stream[i + 1]);
+                        tempArray.Add(stream[i + 2]);
+                        break;
+                    case "d":
+                        tempArray.Add(stream[i + 4]);
+                        tempArray.Add(stream[i + 1]);
+                        tempArray.Add(stream[i + 2]);
+                        tempArray.Add(stream[i + 3]);
+                        break;
+                }
+
+                MessageBox.Show(tempArray[0] + "\n" + tempArray.Count.ToString(), "array contents", MessageBoxButton.OK);
+
+                using (SQLiteConnection conn = new SQLiteConnection(@"DataSource=" + databasename))
+                {
+                    conn.Open();
+                    SQLiteCommand command = new SQLiteCommand("INSERT INTO questions(Question,Answer1,Answer2,Answer3,Answer4) VALUES(?,?,?,?,?)", conn);
+                    command.Parameters.Add("@param1", System.Data.DbType.String).Value = tempArray[0];
+                    command.Parameters.Add("@param2", System.Data.DbType.String).Value = tempArray[1];
+                    command.Parameters.Add("@param3", System.Data.DbType.String).Value = tempArray[2];
+                    command.Parameters.Add("@param4", System.Data.DbType.String).Value = tempArray[3];
+                    command.Parameters.Add("@param5", System.Data.DbType.String).Value = tempArray[4];
+
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception(ex.Message);
+                    }
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Win32;
+using MultipleChoiceMVVM.Model;
+
+namespace MultipleChoiceMVVM.ViewModel
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private RelayCommand _loadTest;
+        private RelayCommand _submitAnswer;
+        private RelayCommand _loadNewQuestions;
+        private string _question;
+        private string _answera;
+        private string _answerb;
+        private string _answerc;
+        private string _answerd;
+        private string correct_answer;
+
+        private bool _rdoanswera;
+        private bool _rdoanswerb;
+        private bool _rdoanswerc;
+        private bool _rdoanswerd;
+
+        private bool _isAnswered;
+
+        CancellationTokenSource cts;
+
+        public ICommand LoadTest
+        {
+            get
+            {
+                if (_loadTest == null)
+                {
+                    _loadTest = new RelayCommand(param => this.FireTestAsync());
+                }
+                return _loadTest;
+            }
+        }
+
+        public ICommand SubmitAnswer
+        {
+            get
+            {
+                if(_submitAnswer == null)
+                {
+                    _isAnswered = true;
+                    _submitAnswer = new RelayCommand(param => this.AnswerQuestion());
+                }
+                return _submitAnswer;
+            }
+        }
+
+        public ICommand LoadNewQuestions
+        { 
+            get
+            {
+                if (_loadNewQuestions == null)
+                {
+                    _loadNewQuestions = new RelayCommand(param => this.LoadQuestions());
+                }
+                return _loadNewQuestions;
+            }
+        }
+
+        public void LoadQuestions()
+        {
+            DataModel dm = new DataModel();
+            // open the document
+            // read all lines in to list
+            // call the function to import the database
+            List<string> data = new List<string>();
+            string filename = "";
+            string databasename = "";
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "%USERDIR%";
+            ofd.Title = "Open Questions";
+            ofd.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (ofd.ShowDialog() == true)
+            {
+                filename = ofd.FileName;
+                
+                string[] lines = File.ReadAllLines(filename, Encoding.UTF8);
+                foreach (string line in lines)
+                {
+                    data.Add(line);
+                }
+            }
+                      
+
+            ofd.Title = "Open Database";
+            ofd.InitialDirectory = "%USERDIR%";
+            ofd.Filter = "Database Files (*.db)|*.db";
+            if (ofd.ShowDialog() == true)
+            {
+                databasename = ofd.FileName;
+            }
+
+
+            dm.SaveQuestionsToDatabase(data, databasename);
+        }
+
+
+        public void AnswerQuestion()
+        {
+            if (Rdoanswera)
+            {
+                if (Answera.Equals(correct_answer))
+                {
+                    MessageBox.Show("Correct", "Correct", MessageBoxButton.OK);
+                    cts.Cancel();
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect", "Wrong", MessageBoxButton.OK);
+                    cts.Cancel();
+                }
+            }
+            else if(Rdoanswerb)
+            {
+                if (Answerb.Equals(correct_answer))
+                {
+                    MessageBox.Show("Correct", "Correct", MessageBoxButton.OK);
+                    cts.Cancel();
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect", "Wrong", MessageBoxButton.OK);
+                    cts.Cancel();
+                }
+            }
+            else if (Rdoanswerc)
+            {
+                if (Answerc.Equals(correct_answer))
+                {
+                    MessageBox.Show("Correct", "Correct", MessageBoxButton.OK);
+                    cts.Cancel();
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect", "Wrong", MessageBoxButton.OK);
+                    cts.Cancel();
+                }
+            }
+            else if (Rdoanswerd)
+            {
+                if (Answerd.Equals(correct_answer))
+                {
+                    MessageBox.Show("Correct", "Correct", MessageBoxButton.OK);
+                    cts.Cancel();
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect", "Wrong", MessageBoxButton.OK);
+                    cts.Cancel();
+                }
+            }
+        }
+
+        public async Task FireTestAsync()
+        {
+            DataModel dm = new DataModel();
+
+            string databasename = "";
+            IsAnswered = false;
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                databasename = ofd.FileName;
+            }
+            List<List<string>> data = new List<List<string>>();
+
+            data = dm.ConnectToDataBase(databasename);
+
+            data = data.OrderBy(a => Guid.NewGuid()).ToList();
+
+            for(int i = 0; i < data.Count; i++)
+            {
+                cts = new CancellationTokenSource();
+                DisplayQuestion(data[i]);
+                await WaitToComplete();
+            }
+        }
+
+        async Task WaitToComplete()
+        {
+            try
+            {
+                await Task.Delay(90000, cts.Token);
+            }
+            catch (Exception ex)
+            {
+                // MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK);
+                Debug.WriteLine(ex);
+
+            }
+        }
+
+        private void DisplayQuestion(List<string> data)
+        {
+            Question = data[1];
+
+            List<string> answers = new List<string>();
+            answers.Add(data[2]);
+            answers.Add(data[3]);
+            answers.Add(data[4]);
+            answers.Add(data[5]);
+
+            correct_answer = data[2];
+
+            answers = answers.OrderBy(a => Guid.NewGuid()).ToList();
+            Answera = answers[0];
+            Answerb = answers[1];
+            Answerc = answers[2];
+            Answerd = answers[3];
+        }
+
+        public string Question
+        {
+            get { return _question; }
+            set
+            {
+                if (_question != value)
+                {
+                    _question = value;
+                    RaisePropertyChanged("Question");
+                }
+            }
+        }
+
+        public string Answera
+        {
+            get { return _answera; }
+            set
+            {
+                if (_answera != value)
+                {
+                    _answera = value;
+                    RaisePropertyChanged("Answera");
+                }
+            }
+        }
+        public string Answerb
+        {
+            get { return _answerb; }
+            set
+            {
+                if (_answerb != value)
+                {
+                    _answerb = value;
+                    RaisePropertyChanged("Answerb");
+                }
+            }
+        }
+        public string Answerc
+        {
+            get { return _answerc; }
+            set
+            {
+                if (_answerc != value)
+                {
+                    _answerc = value;
+                    RaisePropertyChanged("Answerc");
+                }
+            }
+        }
+        public string Answerd
+        {
+            get { return _answerd; }
+            set
+            {
+                if (_answerd != value)
+                {
+                    _answerd = value;
+                    RaisePropertyChanged("Answerd");
+                }
+            }
+        }
+
+        public bool Rdoanswera
+        {
+            get { return _rdoanswera; }
+            set
+            {
+                if(_rdoanswera != value)
+                {
+                    _rdoanswera = value;
+                    RaisePropertyChanged("Rdoanswera");
+                }
+            }
+        }
+        public bool Rdoanswerb
+        {
+            get { return _rdoanswerb; }
+            set
+            {
+                if (_rdoanswerb != value)
+                {
+                    _rdoanswerb = value;
+                    RaisePropertyChanged("Rdoanswera");
+                }
+            }
+        }
+        public bool Rdoanswerc
+        {
+            get { return _rdoanswerc; }
+            set
+            {
+                if (_rdoanswerc != value)
+                {
+                    _rdoanswerc = value;
+                    RaisePropertyChanged("Rdoanswera");
+                }
+            }
+        }
+        public bool Rdoanswerd
+        {
+            get { return _rdoanswerd; }
+            set
+            {
+                if (_rdoanswerd != value)
+                {
+                    _rdoanswerd = value;
+                    RaisePropertyChanged("Rdoanswera");
+                }
+            }
+        }
+
+        public bool IsAnswered
+        {
+            get { return _isAnswered; }
+            set
+            {
+                if (_isAnswered != value)
+                {
+                    _isAnswered = value;
+                    RaisePropertyChanged("IsAnswered");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
